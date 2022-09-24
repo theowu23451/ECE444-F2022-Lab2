7df6776df21f1ab334100a62ff815d03ac7c9599
Theo Wu <theowu23451@gmail.com>
2022-09-24 02:01:54 -0400

    Add email form field with validation for being UofT

--- hello_form.py
+++ hello_form.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my secret string'
@@ -14,6 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email(granular_message=True)])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -29,8 +30,16 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
         session['name'] = form.name.data
+        if "utoronto" in form.email.data:
+            session['email'] = form.email.data
+        else:
+            session['email'] = None
         return redirect(url_for('index'))
-    return render_template('index_form.html', form=form, name=session.get('name'))
+    return render_template('index_form.html', form=form, 
+        name=session.get('name'), email=session.get('email'))
